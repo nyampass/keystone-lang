c67f1827d4f3b285a82224fe5b79fb2c98013f52
Tokusei Noborio <tokusei@nyampass.com>
2023-01-04 21:48:22 +0900

    wip: if

--- src/keystone.clj
+++ src/keystone.clj
@@ -6,17 +6,15 @@
 (def parser
   (insta/parser
    "s ::= block
-    block ::= {stat}
-    
-    <stat> ::= <space> ( define | op | loop | functioncall | label | goto name | do block end | while exp do block end | repeat block until exp | if exp then block {elseif exp then block} [else block] end | for name '=' exp ',' exp [',' exp] do block end | for namelist in explist do block end | function funcname funcbody | local function name funcbody | local namelist ['=' explist] ) <'\n'*>
-  
-  <space> ::= #\"\\s*\"
-    
+  block ::= {stat}    
+  <stat> ::= <space> ( define | op | loop | functioncall | label | goto name | do block end | while exp do block end | repeat block until exp | if | for name '=' exp ',' exp [',' exp] do block end | for namelist in explist do block end | function funcname funcbody | local function name funcbody | local namelist ['=' explist] ) <'\n'*>
+  <space> ::= #\"\\s*\"    
   op ::= ( print | move ) <space> exp <space> 
   print ::= 'print'
   move ::= 'move'
 	define ::=  name <space> '=' <space> exp
-  loop ::= 'loop' <space> exp <space> <'\n'*> block  <'\n'*> end  <'\n'*>
+  loop ::= 'loop' <space> exp <space> <'\n'*> block <'\n'*> end
+  if ::= 'if' <space> exp <space> <'\n'*> block end
 
   goto ::= 'goto'
   do ::= 'do'
@@ -24,7 +22,6 @@
   while ::= 'white'
   repeat ::= 'repeat'
   until ::= 'until'
-  if ::= 'if'
   then ::= 'then'
   elseif ::= 'elseif'
   else ::= 'else'
@@ -50,7 +47,7 @@
 
 	explist ::= exp {',' exp}
 
-	<exp> ::=  name | nil | false | true | numeral | literal-string | '...' | functiondef | tableconstructor | exp binop exp | unop exp
+	<exp> ::=  name | nil | false | true | numeral | literal-string | '...' | functiondef | exp <space> binop <space> exp | unop exp
  
   literal-string ::= '\"' #\"[^\\\"]+\" '\"'
  
@@ -58,21 +55,13 @@
  
 functioncall ::=  name args
 
-	args ::=  '(' [explist] ')' | tableconstructor | literal-string
+	args ::=  '(' [explist] ')' | literal-string
 
 	functiondef ::= function funcbody
 
 	funcbody ::= '(' [parlist] ')' block end
 
 	parlist ::= namelist [',' '...'] | '...'
-
-	tableconstructor ::= '{' [fieldlist] '}'
-
-	fieldlist ::= field {fieldsep field} [fieldsep]
-
-	field ::= '[' exp ']' '=' exp | name '=' exp | exp
-
-	fieldsep ::= ',' | ';'
 
 	binop ::=  '+' | '-' | '*' | '/' | '//' | '^' | '%' | '&' | '~' | '|' | '>>' | '<<' | '..' | '<' | '<=' | '>' | '>=' | '==' | '~=' | and | or
 
@@ -99,13 +88,22 @@ functioncall ::=  name args
 (defn -numeral [& [val]]
   (parse-long val))
 
+(defn -loop [& args]
+  (prn :-loop args)
+  args)
+
+(defn -if [& args]
+  (prn :-if args)
+  args)
+
 (defn transform [stats]
   (insta/transform {:block (fn [& args] args)
                     :op -op
                     :define -define
                     :name -name
                     :literal-string -literal-string
-                    :numeral -numeral} stats))
+                    :numeral -numeral
+                    :loop -loop} stats))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn run [{:keys [code]}]
